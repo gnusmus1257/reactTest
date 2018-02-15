cb63d29519d4cd74ffe5952ea05e6de76a3a433c
Artyom <artemilyin2013@gmail.com>
2018-02-15 18:35:55 +0300

    first icons completed

--- js/index.jsx
+++ js/index.jsx
@@ -3,10 +3,14 @@ class Card extends React.Component {
         return (
         <div className={"card"}>
             <div className={"desktop"}>
-
+                <img src={"../Content/Images/Icons/Apple Music.png"} className={"desktop-icon"}/>
+                <img src={"../Content/Images/Icons/Safari.png"} className={"desktop-icon"}/>
+                <img src={"../Content/Images/Icons/Settings.png"} className={"desktop-icon"}/>
+                <img src={"../Content/Images/Icons/Wallet2.png"} className={"desktop-icon"}/>
+                <img src={"../Content/Images/Icons/Photos.png"} className={"desktop-icon"}/>
             </div>
             <div className={"footer"}>
-                <img src={""}/>
+
             </div>
         </div>)
     }
