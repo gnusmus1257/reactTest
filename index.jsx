6e4cd8ce561c43b126d6213b31b4ab7e17eaa15c
Artyom <artemilyin2013@gmail.com>
2018-02-15 17:42:59 +0300

    attach ios icons, create footer with filter

--- index.jsx
+++ index.jsx
@@ -2,7 +2,6 @@ class Card extends React.Component {
     render() {
         return (
         <div className={"card"}>
-            <img src={"bg.jpg"} className={"responsive "}/>
             <div className={"desktop"}>
 
             </div>
